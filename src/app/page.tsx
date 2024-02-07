f27b495be210a0337524a9df98242f540e8b1bd1
Vitor Prata <vitor.prata@allmatech.com.br>
2024-02-07 12:29:07 -0300

    Criação do banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import { Inter } from 'next/font/google';
 import styles from './page.module.css';
 import Link from 'next/link';
 
+import { Banner } from "@/features/home"
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
-      
+      <Banner />
     </main>
   );
 }
--- src/features/home/banner.tsx
+++ src/features/home/banner.tsx
@@ -1,11 +1,44 @@
+'use client';
+
 import Link from "next/link";
 
 const Banner = () => {
     
     return (
-        <>
-            
-        </>
+        <div>
+            <div className="m-auto flex justify-between items-center max-w-[1140px]">
+                <div className="w-[50%]">
+                    <div>
+                        <h2 className="uppercase tracking-widest bg-opacity rounded-md px-3 w-fit">Vitor Prata</h2>
+                    </div>
+
+                    <div className="mt-4">
+                        <h1 className="uppercase text-6xl font-semibold">Hello! I{'\u2019'}m Vitor</h1>
+                        <h1 className="uppercase text-6xl text-blue-500 mt-2">I{'\u2019'}m a developer</h1>
+                    </div>
+
+                    <p className="mt-8 text-lg">
+                        Yet bed any for travelling assistance indulgence unpleasing. 
+                        Not thoughts all exercise blessing. Indulgence way everything 
+                        joy alteration boisterous the attachment.
+                    </p>
+
+                    <div className="flex justify-start mt-6 gap-6 items-center">
+                        <Link href="/contact-me" className="transition-all hover:bg-blue-800 bg-blue-500 rounded-xl text-white py-4 px-8">
+                            Contact Me
+                        </Link>
+
+                        <Link href={window.location.href + '/teste.pdf'} target="_blank" className="line-below">
+                            Download CV
+                        </Link>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+        
     );
 }
 
